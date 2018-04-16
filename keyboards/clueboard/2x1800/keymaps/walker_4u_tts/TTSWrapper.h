3daf2e365c05471ed3150ad8ec6a9f362eb571ed
Michael Walker <walkerstop@gmail.com>
2018-04-16 02:21:49 -0700

    Add TTS layout

---
+++ keyboards/clueboard/2x1800/keymaps/walker_4u_tts/TTSWrapper.h
@@ -0,0 +1,6 @@
+#define byte char
+
+void TTSInit(void);
+void sayPhonemes(const char *phonemes);
+void sayText(const char *phonemes);
+void setPitch(byte pitch);
---
+++ keyboards/clueboard/2x1800/keymaps/walker_4u_tts/tts.mk
@@ -0,0 +1,3 @@
+USB_HID_DIR = protocol/usb_hid
+ARDUINO_DIR = $(USB_HID_DIR)/arduino-1.0.1
+VPATH += $(TMK_DIR)/$(ARDUINO_DIR)/variants/leonardo
