767b916814ed574f2c75aec1c87d022800c3b399
Michael Walker <walkerstop@gmail.com>
2018-04-18 11:12:42 -0700

    Stuff

---
+++ keyboards/clueboard/2x1800/keymaps/walker_4u_tts/TTS/analog.c
@@ -0,0 +1,312 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include "progmem.h"
+#include "pincontrol.h"
+
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+#define NOT_ON_TIMER 0
+#define TIMER0A 1
+#define TIMER0B 2
+#define TIMER1A 3
+#define TIMER1B 4
+#define TIMER2  5
+#define TIMER2A 6
+#define TIMER2B 7
+
+#define TIMER3A 8
+#define TIMER3B 9
+#define TIMER3C 10
+#define TIMER4A 11
+#define TIMER4B 12
+#define TIMER4C 13
+#define TIMER4D 14      
+#define TIMER5A 15
+#define TIMER5B 16
+#define TIMER5C 17
+
+/* Delay for the given number of microseconds.  Assumes a 8 or 16 MHz clock. */
+void delayMicroseconds(unsigned int us)
+{
+        // calling avrlib's delay_us() function with low values (e.g. 1 or
+        // 2 microseconds) gives delays longer than desired.
+        //delay_us(us);
+#if F_CPU >= 20000000L
+        // for the 20 MHz clock on rare Arduino boards
+
+        // for a one-microsecond delay, simply wait 2 cycle and return. The overhead
+        // of the function call yields a delay of exactly a one microsecond.
+        __asm__ __volatile__ (
+                "nop" "\n\t"
+                "nop"); //just waiting 2 cycle
+        if (--us == 0)
+                return;
+
+        // the following loop takes a 1/5 of a microsecond (4 cycles)
+        // per iteration, so execute it five times for each microsecond of
+        // delay requested.
+        us = (us<<2) + us; // x5 us
+
+        // account for the time taken in the preceeding commands.
+        us -= 2;
+
+#elif F_CPU >= 16000000L
+        // for the 16 MHz clock on most Arduino boards
+
+        // for a one-microsecond delay, simply return.  the overhead
+        // of the function call yields a delay of approximately 1 1/8 us.
+        if (--us == 0)
+                return;
+
+        // the following loop takes a quarter of a microsecond (4 cycles)
+        // per iteration, so execute it four times for each microsecond of
+        // delay requested.
+        us <<= 2;
+
+        // account for the time taken in the preceeding commands.
+        us -= 2;
+#else
+        // for the 8 MHz internal clock on the ATmega168
+
+        // for a one- or two-microsecond delay, simply return.  the overhead of
+        // the function calls takes more than two microseconds.  can't just
+        // subtract two, since us is unsigned; we'd overflow.
+        if (--us == 0)
+                return;
+        if (--us == 0)
+                return;
+
+        // the following loop takes half of a microsecond (4 cycles)
+        // per iteration, so execute it twice for each microsecond of
+        // delay requested.
+        us <<= 1;
+
+        // partially compensate for the time taken by the preceeding commands.
+        // we can't subtract any more than this or we'd overflow w/ small delays.
+        us--;
+#endif
+        // busy wait
+        __asm__ __volatile__ (
+                "1: sbiw %0,1" "\n\t" // 2 cycles
+                "brne 1b" : "=w" (us) : "0" (us) // 2 cycles
+        );
+}
+
+
+const uint8_t PROGMEM digital_pin_to_timer_PGM[16] = {
+        NOT_ON_TIMER,
+        NOT_ON_TIMER,
+        NOT_ON_TIMER,
+        TIMER0B,                /* 3 */
+        NOT_ON_TIMER,
+        TIMER3A,                /* 5 */
+        TIMER4D,                /* 6 */
+        NOT_ON_TIMER,
+
+        NOT_ON_TIMER,
+        TIMER1A,                /* 9 */
+        TIMER1B,                /* 10 */
+        TIMER0A,                /* 11 */
+
+        NOT_ON_TIMER,
+        TIMER4A,                /* 13 */
+
+        NOT_ON_TIMER,
+        NOT_ON_TIMER,
+};
+
+#define digitalPinToTimer(P) ( pgm_read_byte( digital_pin_to_timer_PGM + (P) ) )
+
+// Right now, PWM output only works on the pins with
+// hardware support.  These are defined in the appropriate
+// pins_*.c file.  For the rest of the pins, we default
+// to digital output.
+void analogWrite(uint8_t pin, int val)
+{
+	// We need to make sure the PWM output is enabled for those pins
+	// that support it, as we turn it off when digitally reading or
+	// writing with them.  Also, make sure the pin is in output mode
+	// for consistenty with Wiring, which doesn't require a pinMode
+	// call for the analog output pins.
+	pinMode(pin, PinDirectionOutput);
+	if (val == 0)
+	{
+		digitalWrite(pin, PinLevelLow);
+	}
+	else if (val == 255)
+	{
+		digitalWrite(pin, PinLevelHigh);
+	}
+	else
+	{
+		switch(digitalPinToTimer(pin))
+		{
+			// XXX fix needed for atmega8
+			#if defined(TCCR0) && defined(COM00) && !defined(__AVR_ATmega8__)
+			case TIMER0A:
+				// connect pwm to pin on timer 0
+				sbi(TCCR0, COM00);
+				OCR0 = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR0A) && defined(COM0A1)
+			case TIMER0A:
+				// connect pwm to pin on timer 0, channel A
+				sbi(TCCR0A, COM0A1);
+				OCR0A = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR0A) && defined(COM0B1)
+			case TIMER0B:
+				// connect pwm to pin on timer 0, channel B
+				sbi(TCCR0A, COM0B1);
+				OCR0B = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR1A) && defined(COM1A1)
+			case TIMER1A:
+				// connect pwm to pin on timer 1, channel A
+				sbi(TCCR1A, COM1A1);
+				OCR1A = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR1A) && defined(COM1B1)
+			case TIMER1B:
+				// connect pwm to pin on timer 1, channel B
+				sbi(TCCR1A, COM1B1);
+				OCR1B = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR2) && defined(COM21)
+			case TIMER2:
+				// connect pwm to pin on timer 2
+				sbi(TCCR2, COM21);
+				OCR2 = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR2A) && defined(COM2A1)
+			case TIMER2A:
+				// connect pwm to pin on timer 2, channel A
+				sbi(TCCR2A, COM2A1);
+				OCR2A = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR2A) && defined(COM2B1)
+			case TIMER2B:
+				// connect pwm to pin on timer 2, channel B
+				sbi(TCCR2A, COM2B1);
+				OCR2B = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR3A) && defined(COM3A1)
+			case TIMER3A:
+				// connect pwm to pin on timer 3, channel A
+				sbi(TCCR3A, COM3A1);
+				OCR3A = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR3A) && defined(COM3B1)
+			case TIMER3B:
+				// connect pwm to pin on timer 3, channel B
+				sbi(TCCR3A, COM3B1);
+				OCR3B = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR3A) && defined(COM3C1)
+			case TIMER3C:
+				// connect pwm to pin on timer 3, channel C
+				sbi(TCCR3A, COM3C1);
+				OCR3C = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR4A)
+			case TIMER4A:
+				//connect pwm to pin on timer 4, channel A
+				sbi(TCCR4A, COM4A1);
+				#if defined(COM4A0)		// only used on 32U4
+				cbi(TCCR4A, COM4A0);
+				#endif
+				OCR4A = val;	// set pwm duty
+				break;
+			#endif
+			
+			#if defined(TCCR4A) && defined(COM4B1)
+			case TIMER4B:
+				// connect pwm to pin on timer 4, channel B
+				sbi(TCCR4A, COM4B1);
+				OCR4B = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR4A) && defined(COM4C1)
+			case TIMER4C:
+				// connect pwm to pin on timer 4, channel C
+				sbi(TCCR4A, COM4C1);
+				OCR4C = val; // set pwm duty
+				break;
+			#endif
+				
+			#if defined(TCCR4C) && defined(COM4D1)
+			case TIMER4D:				
+				// connect pwm to pin on timer 4, channel D
+				sbi(TCCR4C, COM4D1);
+				#if defined(COM4D0)		// only used on 32U4
+				cbi(TCCR4C, COM4D0);
+				#endif
+				OCR4D = val;	// set pwm duty
+				break;
+			#endif
+
+							
+			#if defined(TCCR5A) && defined(COM5A1)
+			case TIMER5A:
+				// connect pwm to pin on timer 5, channel A
+				sbi(TCCR5A, COM5A1);
+				OCR5A = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR5A) && defined(COM5B1)
+			case TIMER5B:
+				// connect pwm to pin on timer 5, channel B
+				sbi(TCCR5A, COM5B1);
+				OCR5B = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR5A) && defined(COM5C1)
+			case TIMER5C:
+				// connect pwm to pin on timer 5, channel C
+				sbi(TCCR5A, COM5C1);
+				OCR5C = val; // set pwm duty
+				break;
+			#endif
+
+			case NOT_ON_TIMER:
+			default:
+				if (val < 128) {
+					digitalWrite(pin, PinLevelLow);
+				} else {
+					digitalWrite(pin, PinLevelHigh);
+				}
+		}
+	}
+}
+
