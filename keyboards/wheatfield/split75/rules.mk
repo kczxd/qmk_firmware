02977b6d6fa8d3552fcf11a6f5870d5f65282e9f
mwalker <michael.l.walker@intel.com>
2019-05-16 03:41:44 -0700

    More massaging

--- keyboards/wheatfield/split75/rules.mk
+++ keyboards/wheatfield/split75/rules.mk
@@ -48,4 +48,4 @@ CUSTOM_MATRIX = yes
 SRC = matrix.c i2c.c
 
 # programming options
-PROGRAM_CMD = ./keyboards/ps2avrGB/program $(TARGET).hex
+PROGRAM_CMD = ./keyboards/wheatfield/split75/program $(TARGET).hex
